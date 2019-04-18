f4205f925195eaae44b966d06f3ca6bb6b381f5d
MarcillaudG <guilhem.marcillaud@gmail.com>
2019-04-18 11:04:39 +0200

    Create the class MessageCriticality

---
+++ AMASECF/src/messages/MessageCriticality.java
@@ -0,0 +1,21 @@
+package messages;
+/**
+ * 
+ * @author gmarcill
+ *
+ *Message used to share the criticality between agent
+ *
+ */
+public class MessageCriticality {
+
+	
+	private double criticality;
+	
+	public MessageCriticality(double value) {
+		this.criticality = value;
+	}
+	
+	public double getCriticality() {
+		return this.criticality;
+	}
+}
